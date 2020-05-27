31f552322096022490d71c0e0f5fa62e839f4b7d
Harriet Dummett <hattie.dummett@gmail.com>
2020-05-27 20:31:00 +0100

    amend profit calculations

--- spreadsheet-analysis.py
+++ spreadsheet-analysis.py
@@ -1,38 +1,28 @@
 import csv
 
-with open('sales.csv', 'r') as csv_file:
-    spreadsheet = csv.DictReader(csv_file)
+# with open('sales.csv', 'r') as csv_file:
+    # spreadsheet = csv.DictReader(csv_file)
 
-    for row in spreadsheet:
-        print(row['year'], row['month'], row['sales'], row['expenditure'])
+    # for row in spreadsheet:
+        # print(row['year'], row['month'], row['sales'], row['expenditure'])
 
 with open('sales.csv', 'r') as csv_file:
     spreadsheet = csv.DictReader(csv_file)
 
     all_sales = []
+    all_expenditure = []
     for row in spreadsheet:
         all_sales.append(int(row['sales']))
-    print('Sales by month: {}.'.format(all_sales))
-
-
-with open('sales.csv', 'r') as csv_file:
-    spreadsheet = csv.DictReader(csv_file)
-
-    all_expenditures = []
-    for row in spreadsheet:
-        all_expenditures.append(int(row['expenditure']))
-    print('Expenditures by month: {}.'.format(all_expenditures))
-
-all_profits = set(all_sales) - set(all_expenditures)
-all_profits = list(all_profits)
-print('Profit by month: {}'.format(all_profits))
+        all_expenditure.append(int(row['expenditure']))
+    # print(all_sales)
 
 total_sales = sum(all_sales)
-print('Total sales: {}'.format(total_sales))
-
-total_expenditure = sum(all_expenditures)
-print('Total expenditure: {}'.format(total_expenditure))
-
+total_expenditure = sum(all_expenditure)
 total_profit = total_sales - total_expenditure
-print('Total profit: {}'.format(total_profit))
+print("The total sales for 2018 is £" + str(total_sales) + ".")
+print("The total expenditure for 2018 is £" + str(total_expenditure) + ".")
+print("The profit for 2018 is £" + str(total_profit) + ".")
 
+
+# all_profits = set(all_sales) - set(all_expenditures)
+# all_profits = list(all_profits)
