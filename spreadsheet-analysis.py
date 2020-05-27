7a7a4b320d6d4a1d3b2cdac1c3630670e8a48afe
Harriet Dummett <hattie.dummett@gmail.com>
2020-05-27 19:57:36 +0100

    step 3

--- spreadsheet-analysis.py
+++ spreadsheet-analysis.py
@@ -13,3 +13,6 @@ with open('sales.csv', 'r') as csv_file:
     for row in spreadsheet:
         all_sales.append(int(row['sales']))
     print(all_sales)
+
+total_sales = sum(all_sales)
+print(total_sales)
