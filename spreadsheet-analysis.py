7ce98289ad786276b5885fc692decf44eed6feec
Harriet Dummett <hattie.dummett@gmail.com>
2020-05-27 19:46:35 +0100

    update

--- spreadsheet-analysis.py
+++ spreadsheet-analysis.py
@@ -1,8 +1,8 @@
 import csv
 
-field_names = ['year', 'month', 'sales', 'expenditure']
+#  = ['year', 'month', 'sales', 'expenditure']
 
 with open('sales.csv', 'r') as csv_file:
-    spreadsheet = csv.DictReader(csv_file, field_names)
-
-print(spreadsheet)
+    spreadsheet = csv.DictReader(csv_file)
+    for row in spreadsheet:
+        print(dict(row))
