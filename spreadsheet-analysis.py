3fde8d770cc7f4d049ec4e30bc73ff24697bc280
Harriet Dummett <hattie.dummett@gmail.com>
2020-05-27 19:51:08 +0100

    step 2

--- spreadsheet-analysis.py
+++ spreadsheet-analysis.py
@@ -4,5 +4,9 @@ import csv
 
 with open('sales.csv', 'r') as csv_file:
     spreadsheet = csv.DictReader(csv_file)
+
+    all_sales = []
     for row in spreadsheet:
-        print(dict(row))
+        # print(row['year'], row['month'], row['sales'], row['expenditure'])
+        all_sales.append(int(row['sales']))
+    print(all_sales)
