13aea885bcb907e4c62967ffb3bac2994a19cab7
Harriet Dummett <hattie.dummett@gmail.com>
2020-05-27 19:55:32 +0100

    delete rps

--- spreadsheet-analysis.py
+++ spreadsheet-analysis.py
@@ -1,12 +1,15 @@
 import csv
 
-#  = ['year', 'month', 'sales', 'expenditure']
+with open('sales.csv', 'r') as csv_file:
+    spreadsheet = csv.DictReader(csv_file)
+
+    for row in spreadsheet:
+        print(row['year'], row['month'], row['sales'], row['expenditure'])
 
 with open('sales.csv', 'r') as csv_file:
     spreadsheet = csv.DictReader(csv_file)
 
     all_sales = []
     for row in spreadsheet:
-        # print(row['year'], row['month'], row['sales'], row['expenditure'])
         all_sales.append(int(row['sales']))
     print(all_sales)
